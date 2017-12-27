a8ca384a6d831248311377c806c9699de3148151
NataKr <finka1983@gmail.com>
2017-12-27 10:47:43 +0200

    added init function

--- js/myjs.js
+++ js/myjs.js
@@ -1,1 +1,19 @@
-console.log("yo");
+//console.log("yo");
+var ticTacToe={};
+
+ticTacToe.init=function(){
+    ticTacToe.currentPlayer="red";
+    ticTacToe.playerIndicator=$(".current-player-indicator");
+    ticTacToe.playerIndicator.addClass(ticTacToe.currentPlayer);
+    ticTacToe.boxes=$(".box");
+    ticTacToe.boxes.on("click", ticTacToe.clickOnBoxes);
+};
+
+ticTacToe.clickOnBoxes=function(){
+    $(this).addClass(ticTacToe.currentPlayer);
+    console.log("box clicked");
+}
+
+$(document).ready(function(){
+    ticTacToe.init();
+});
